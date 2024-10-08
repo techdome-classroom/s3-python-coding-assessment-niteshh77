212dcff9e0915fcce5fcb70a7c434e1f21962b7a
YogeshKumar805 <bosyogeshkumar@gmail.com>
2024-10-08 15:29:09 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,9 +1,18 @@
 class Solution(object):
     def isValid(self, s):
-        st=[]
-        for i in s: 
-            if i=='('
+        stack = [] # only use append and pop
+        pairs = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+        for bracket in s:
+            if bracket in pairs:
+                stack.append(bracket)
+            elif len(stack) == 0 or bracket != pairs[stack.pop()]:
+                return False
 
+        return len(stack) == 0
 
 
 
